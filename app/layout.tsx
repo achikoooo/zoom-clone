b257ee0d45c7fcd625d68c7d7471d03cccdf442b
archil <sitsocker@GMAIL.COM>
2024-06-21 08:44:37 +0400

    shchadule

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
+import '@stream-io/video-react-sdk/dist/css/styles.css'
+import 'react-datepicker/dist/react-datepicker.css'
+
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from '@/components/ui/toaster'
-import '@stream-io/video-react-sdk/dist/css/styles.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
